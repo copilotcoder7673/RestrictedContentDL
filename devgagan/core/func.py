c50d773d6b8f0d28e129538d61b0aec487e43e4b
copilotcoder7673 <abirarafat1996@gmail.com>
2025-03-29 05:36:30 +0600

    Update func.py

--- devgagan/core/func.py
+++ devgagan/core/func.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------
-# File Name: func.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import math
 import time , re
 from pyrogram import enums
@@ -250,7 +236,7 @@ async def progress_callback(current, total, progress_message):
     f"│ **__Progress:__** {percent:.2f}%\n"
     f"│ **__Uploaded:__** {current_mb:.2f} MB / {total_mb:.2f} MB\n"
     f"╰──────────────────╯\n\n"
-    f"**__Powered by Team SPY__**"
+    f"**__Powered by @abirxdhackz__**"
         )
 
         last_update_time = current_time
