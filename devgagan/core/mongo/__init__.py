113755a5febb2c50ee11ba0c273f31c8b75c1006
copilotcoder7673 <abirarafat1996@gmail.com>
2025-03-29 05:37:29 +0600

    Update __init__.py

--- devgagan/core/mongo/__init__.py
+++ devgagan/core/mongo/__init__.py
@@ -2,10 +2,10 @@
 # File Name: __init__.py
 # Description: A Pyrogram bot for downloading files from Telegram channels or groups 
 #              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
+# Author: Abir Arafat Chawdhury
+# GitHub: https://github.com/abirxdhack
+# Telegram: https://t.me/abirxdhackz
+# YouTube: https://youtube.com/@abirxdhackz
 # Created: 2025-01-11
 # Last Modified: 2025-01-11
 # Version: 2.0.5
