71da596bd8b92b352183b72f04e9bdc3031a92f6
copilotcoder7673 <abirarafat1996@gmail.com>
2025-03-29 06:03:04 +0600

    Update config.py

--- config.py
+++ config.py
@@ -34,8 +34,8 @@ YTUB_COOKIES = """
 .youtube.com	TRUE	/	TRUE	1777766048	PREF	f6=80&tz=Asia.Dhaka
 """
 
-API_ID = int(getenv("API_ID", "7fc5b35692454973318b86481ab5eca3"))
-API_HASH = getenv("API_HASH", "28239710")
+API_ID = int(getenv("API_ID", "28239710"))
+API_HASH = getenv("API_HASH", "7fc5b35692454973318b86481ab5eca3")
 BOT_TOKEN = getenv("BOT_TOKEN", "8019809817:AAHN9iPbP2MNZXsOHK9inNi04DQx8hk0uE4")
 OWNER_ID = list(map(int, getenv("OWNER_ID", "7303810912").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ytpremium4434360:zxx1VPDzGW96Nxm3@itssmarttoolbot.dhsl4.mongodb.net/?retryWrites=true&w=majority&appName=ItsSmartToolBot")
